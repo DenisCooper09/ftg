4dee60f1f8fb2658e022f76df74d4fbcbbb7e815
Denis Cooper <denis.cooper09@gmail.com>
2023-05-16 21:03:01 +0100

    Link SFML into the project, create basic window

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-	std::cout << "Fractal Tree Generator" << std::endl;
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	constexpr int WINDOW_WIDTH = 1200;
+	constexpr int WINDOW_HEIGHT = 800;
+
+	sf::RenderWindow window({WINDOW_WIDTH, WINDOW_HEIGHT},
+							"Fractal Tree Generator",
+							sf::Style::Close,
+							settings);
+
+	while (window.isOpen()) {
+		sf::Event event{};
+
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+				default:
+					break;
+			}
+		}
+
+		window.clear(sf::Color::Black);
+		window.display();
+	}
 
 	return EXIT_SUCCESS;
 }
